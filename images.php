82b8a719ab44dc8a62213ec37be46cc0767de6c4
Eduardo Caron <eduardocaron10@gmail.com>
2015-05-31 16:44:05 -0300

    primeiro envio

---
+++ images.php
@@ -0,0 +1,20 @@
+<?php 
+
+	//converting to base64
+	$path = '/home/eduardo/cisco.png';
+	$type = pathinfo($path, PATHINFO_EXTENSION);
+	$data = file_get_contents($path);
+	$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+
+	//converting to original filetype(.png)
+
+	$base64_string = $base64;
+	$output_file = '/home/eduardo/teste.png';
+
+	$ifp = fopen($output_file, "wb"); 
+
+    $data = explode(',', $base64_string);
+
+    fwrite($ifp, base64_decode($data[1])); 
+    fclose($ifp);  
+ ?>
